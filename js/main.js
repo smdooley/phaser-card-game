ba712a3783b17a6dbda4c87df7d4dd7820525d98
Sean Dooley <sean.dooley.22@gmail.com>
2018-05-29 21:55:30 +0100

    Added FBInstantGame API

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 var App = App || {};
 
-App.game = new Phaser.Game(370, 400, Phaser.AUTO);
+FBInstant.initializeAsync().then(function() {
+    FBInstant.setLoadingProgress(100);
+    FBInstant.startGameAsync().then(function() {
+        App.game = new Phaser.Game(480, 720, Phaser.AUTO);
 
-App.game.state.add('BootState', App.BootState);
-App.game.state.add('PreloadState', App.PreloadState);
-App.game.state.add('GameState', App.GameState);
-App.game.state.add('CompleteState', App.CompleteState);
-App.game.state.add('GameOverState', App.GameOverState);
+        App.game.state.add('BootState', App.BootState);
+        App.game.state.add('PreloadState', App.PreloadState);
+        App.game.state.add('GameState', App.GameState);
+        App.game.state.add('CompleteState', App.CompleteState);
+        App.game.state.add('GameOverState', App.GameOverState);
 
-App.game.state.start('BootState');
+        App.game.state.start('BootState');
+    });
+});
